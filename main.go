a7b6f2d580f6bf92ad774ab14dfe7690f1108010
Manikanth Reddy <manikanthreddy669@gmail.com>
2023-09-21 20:22:38 -0400

    Add multiplesOfNine function

--- main.go
+++ main.go
@@ -5,4 +5,19 @@ import "fmt"
 
 func main() {
 	fmt.Println("Main function")
+	multiplesOfNine()
+}
+
+//Display name,id
+func display(name string, id int) {
+    fmt.Println(name,id)
+}
+
+//Display multiples of 9
+func multiplesOfNine() {
+	display("Manikanth Reddy",500224276)
+	fmt.Println("Multiples of 9")
+	for i:=1;i<10;i++ {
+	fmt.Println(9*i)
+	}
 }
